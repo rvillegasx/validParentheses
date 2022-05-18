f216601f20a2fe690ecf169b40e3550e86763c67
Roberto Villegas <rvillegas@gmail.com>
2022-05-18 16:33:59 -0500

    Ajustando bigint

--- index.js
+++ index.js
@@ -2,39 +2,40 @@ const myArg = process.argv.slice(2);
 
 const param = myArg[0];
 
-let parentesis = 0;
-let corchetes = 0;
-let cuadrados = 0;
+let parentesis = BigInt(0);
+let corchetes = BigInt(0);
+let cuadrados = BigInt(0);
 
 for(let i = 0; i < param.length; i++) {
     if (param.charAt(i) == '(') {
-        parentesis = (parentesis + 1) + (corchetes + cuadrados);
+        parentesis = (parentesis + BigInt(1)) + BigInt(corchetes + cuadrados);
     }
     if (param.charAt(i) == ')') {
         if (parentesis == 0) {
             continue;
         }
-        parentesis = (parentesis - 1) - (corchetes + cuadrados);
+        parentesis = (parentesis - BigInt(1)) - BigInt(corchetes + cuadrados);
     }
     if (param.charAt(i) == '{') {
-        corchetes = (corchetes + 1) + (parentesis + cuadrados);
+        corchetes = (corchetes + BigInt(1)) + BigInt(parentesis + cuadrados);
     }
     if (param.charAt(i) == '}') {
         if (corchetes == 0) {
             continue;
         }
-        corchetes = (corchetes - 1) - (parentesis + cuadrados);
+        corchetes = (corchetes - BigInt(1)) - BigInt(parentesis + cuadrados);
     }
     if (param.charAt(i) == '[') {
-        cuadrados = (cuadrados + 1) + (corchetes + parentesis);
+        cuadrados = (cuadrados + BigInt(1)) + BigInt(corchetes + parentesis);
     }
     if (param.charAt(i) == ']') {
         if (cuadrados == 0) {
             continue;
         }
-        cuadrados = (cuadrados - 1) - (corchetes + parentesis);
+        cuadrados = (cuadrados - BigInt(1)) - BigInt(corchetes + parentesis);
     }
 }
+
 if (parentesis == 0 && cuadrados == 0 && corchetes == 0) {
     console.log('valid');
 } else {
