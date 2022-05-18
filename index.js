1b1d8593a8ece1c77b449fe63e06042677d2fac1
Roberto Villegas <rvillegas@gmail.com>
2022-05-18 16:52:24 -0500

    Bug señalado por mi hijo

--- index.js
+++ index.js
@@ -5,35 +5,41 @@ const param = myArg[0];
 let parentesis = BigInt(0);
 let corchetes = BigInt(0);
 let cuadrados = BigInt(0);
-
-for(let i = 0; i < param.length; i++) {
-    if (param.charAt(i) == '(') {
-        parentesis = (parentesis + BigInt(1)) + BigInt(corchetes + cuadrados);
-    }
-    if (param.charAt(i) == ')') {
-        if (parentesis == 0) {
-            continue;
+if (param.length > 1) {
+    for(let i = 0; i < param.length; i++) {
+        if (param.charAt(i) == '(') {
+            parentesis = (parentesis + BigInt(1)) + BigInt(corchetes + cuadrados);
         }
-        parentesis = (parentesis - BigInt(1)) - BigInt(corchetes + cuadrados);
-    }
-    if (param.charAt(i) == '{') {
-        corchetes = (corchetes + BigInt(1)) + BigInt(parentesis + cuadrados);
-    }
-    if (param.charAt(i) == '}') {
-        if (corchetes == 0) {
-            continue;
+        if (param.charAt(i) == ')') {
+            if (parentesis == 0) {
+                parentesis = BigInt(-1);
+                continue;
+            }
+            parentesis = (parentesis - BigInt(1)) - BigInt(corchetes + cuadrados);
         }
-        corchetes = (corchetes - BigInt(1)) - BigInt(parentesis + cuadrados);
-    }
-    if (param.charAt(i) == '[') {
-        cuadrados = (cuadrados + BigInt(1)) + BigInt(corchetes + parentesis);
-    }
-    if (param.charAt(i) == ']') {
-        if (cuadrados == 0) {
-            continue;
+        if (param.charAt(i) == '{') {
+            corchetes = (corchetes + BigInt(1)) + BigInt(parentesis + cuadrados);
         }
-        cuadrados = (cuadrados - BigInt(1)) - BigInt(corchetes + parentesis);
+        if (param.charAt(i) == '}') {
+            if (corchetes == 0) {
+                corchetes = BigInt(-1);
+                continue;
+            }
+            corchetes = (corchetes - BigInt(1)) - BigInt(parentesis + cuadrados);
+        }
+        if (param.charAt(i) == '[') {
+            cuadrados = (cuadrados + BigInt(1)) + BigInt(corchetes + parentesis);
+        }
+        if (param.charAt(i) == ']') {
+            if (cuadrados == 0) {
+                cuadrados = BigInt(-1);
+                continue;
+            }
+            cuadrados = (cuadrados - BigInt(1)) - BigInt(corchetes + parentesis);
+        }
     }
+} else {
+    parentesis = BigInt(1);
 }
 
 if (parentesis == 0 && cuadrados == 0 && corchetes == 0) {
